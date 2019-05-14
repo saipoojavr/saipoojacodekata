f3f185548f3cff0b419a2ea1716760275dad9590
saipoojavr <50616206+saipoojavr@users.noreply.github.com>
2019-05-14 16:25:13 +0530

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -1,5 +1,8 @@
 t=str(input())
-if(t=='a' or t=='e' or t=='i' or t=='o' or t=='u' ):
-	print("Vowel")
+if(t>='a' and t<='z'):
+	if(t=='a' or t=='e' or t=='i' or t=='o' or t=='u' ):
+		print("Vowel")
+	else:
+		print("Consonant")
 else:
-	print("Consonant")
+	print("invalid")
