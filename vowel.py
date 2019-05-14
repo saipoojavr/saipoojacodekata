507a6e32e761faed53dfecf9ed45f9771b8e0843
saipoojavr <50616206+saipoojavr@users.noreply.github.com>
2019-05-14 16:35:25 +0530

    Add files via upload

--- vowel.py
+++ vowel.py
@@ -1,8 +1,5 @@
 t=str(input())
-if(t>='a' and t<='z'):
-	if(t=='a' or t=='e' or t=='i' or t=='o' or t=='u' ):
-		print("Vowel")
-	else:
-		print("Consonant")
+if(t=='a' or t=='e' or t=='i' or t=='o' or t=='u' ):
+	print("Vowel")
 else:
-	print("invalid")
+	print("Consonant")
