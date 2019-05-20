c412eb56236380866b666aecd00de010bc3259a4
saipoojavr <50616206+saipoojavr@users.noreply.github.com>
2019-05-20 16:27:18 +0530

    Add files via upload

---
+++ even.py
@@ -0,0 +1,11 @@
+try:
+	num1,num2=input().split()
+	num1=int(num1)
+	num2=int(num2)
+	for iter in range(num1+1,num2):
+		if(iter%2==0):
+			print(iter,end=" ")
+		else:
+			continue
+except ValueError:
+	print("invalid")
