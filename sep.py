71e4b4c9aabd85e05420399fc14e8f0dbb891d4e
saipoojavr <50616206+saipoojavr@users.noreply.github.com>
2019-05-22 16:51:59 +0530

    Add files via upload

---
+++ sep.py
@@ -0,0 +1,12 @@
+try:
+	num=int(input())
+	a=0
+	arr=[]
+	while num>0:
+	    a=num%10
+	    arr.append(a)
+	    num=num//10
+	for i in range(-1,-len(arr)-1,-1):
+	    print(arr[i],end=" ")
+except ValueError:
+	print("invalid")
