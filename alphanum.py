fb7a70867414183872e50b0bf8717b31833c0912
saipoojavr <50616206+saipoojavr@users.noreply.github.com>
2019-05-22 17:44:30 +0530

    Add files via upload

---
+++ alphanum.py
@@ -0,0 +1,14 @@
+string=str(input())
+count=0
+count1=0
+for iter in range(0,len(string)):
+	if(string[iter].isalpha()==True):
+		count=count+1
+	elif(string[iter].isdigit()==True):
+		count1=count1+1
+	else:
+		continue
+if(count>0 and count1>0):
+	print("yes")
+else:
+	print("no")
