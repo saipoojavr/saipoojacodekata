ffb3bbbdcf3dde0e30cafc2901711f53cdd0c075
saipoojavr <50616206+saipoojavr@users.noreply.github.com>
2019-05-23 15:08:43 +0530

    Add files via upload

---
+++ min10.py
@@ -0,0 +1,16 @@
+try:
+	n1,n2,n3,n4,n5,n6,n7,n8,n9,n10=input().split()
+	n1=int(n1)
+	n2=int(n2)
+	n3=int(n3)
+	n4=int(n4)
+	n5=int(n5)
+	n5=int(n5)
+	n6=int(n6)
+	n7=int(n7)
+	n8=int(n8)
+	n9=int(n9)
+	n10=int(n10)
+	print(min(n1,n2,n3,n4,n5,n6,n7,n8,n9,n10))
+except ValueError:
+	print("invalid")
