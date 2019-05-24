049cceca6a873267410627f00badc9cce8baba6b
saipoojavr <50616206+saipoojavr@users.noreply.github.com>
2019-05-24 16:01:19 +0530

    Update numalpha.py

--- numalpha.py
+++ numalpha.py
@@ -1,7 +1,11 @@
-astr=list(input())
+astr=str(input())
 l=len(astr)
+count=0
 for i in range(0,l):
 	if(astr[i].isdigit()==True):
 		print(astr[i],end="")
+		count=count+1
 	else:
 		continue
+if(count==0):
+	print(astr,end="")
