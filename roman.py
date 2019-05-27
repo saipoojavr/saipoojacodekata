d198a92fc2f7ef47ea619fe4107ba1abbd2e0c4f
saipoojavr <50616206+saipoojavr@users.noreply.github.com>
2019-05-27 15:38:20 +0530

    Add files via upload

---
+++ roman.py
@@ -0,0 +1,4 @@
+astr=['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XVIII','XIX','XX']
+number=input()
+if number in astr:
+    print(astr.index(number)+1)
