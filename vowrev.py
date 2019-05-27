c3a2ffa64dc551ddc5da6499df02abb77e0bdd71
saipoojavr <50616206+saipoojavr@users.noreply.github.com>
2019-05-27 17:54:15 +0530

    Update vowrev.py

--- vowrev.py
+++ vowrev.py
@@ -1,4 +1,6 @@
+length=int(input())
 astr=str(input())
-for i in range(-1,-len(astr)-1,-1):
+for i in range(-1,-length-1,-1):
 	if not(astr[i]=='a' or astr[i]=='e' or astr[i]=='i' or astr[i]=='o' or astr[i]=='u'):
 		print(astr[i],end="")
+
