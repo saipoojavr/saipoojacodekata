82fa3eaf7c6d7301003434cae0926a5d57df21ea
saipoojavr <50616206+saipoojavr@users.noreply.github.com>
2019-05-28 17:54:55 +0530

    Update nospace.py

--- nospace.py
+++ nospace.py
@@ -1,6 +1,11 @@
 astr=str(input())
 for iter in range(0,len(astr)):
-	if(astr[iter]==" "):
+	if(astr[iter]==" " and astr[iter+1]==" "):
+		print(" ",end="")
+	elif(astr[iter]==" " and astr[iter-1]==" "):
 		continue
 	else:
 		print(astr[iter],end="")
+		
+		
+		
