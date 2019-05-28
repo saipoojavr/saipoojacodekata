3da5346f770d70201076a99167fa68f2593d42bf
saipoojavr <50616206+saipoojavr@users.noreply.github.com>
2019-05-28 17:57:34 +0530

    Update nospace.py

--- nospace.py
+++ nospace.py
@@ -1,11 +1,4 @@
-astr=str(input())
-for iter in range(0,len(astr)):
-	if(astr[iter]==" " and astr[iter+1]==" "):
-		print(" ",end="")
-	elif(astr[iter]==" " and astr[iter-1]==" "):
-		continue
-	else:
-		print(astr[iter],end="")
-		
-		
+import re
+astr=input()
+print(re.sub(' +',' ',astr))
 		
