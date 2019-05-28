8520c52328d70a8302f217d47e74732be70c257d
saipoojavr <50616206+saipoojavr@users.noreply.github.com>
2019-05-28 15:41:34 +0530

    Update char+3.py

--- char+3.py
+++ char+3.py
@@ -1,5 +1,12 @@
 astr=str(input())
 for iter in range(0,len(astr)):
 	asc=ord(astr[iter])
-	asc=asc+3
-	print(chr(asc),end="")
+	if(asc>=65 and asc<88):
+		asc=asc+3
+		print(chr(asc),end="")
+	elif(asc==88):
+		print('A',end="")
+	elif(asc==89):
+		print('B',end="")
+	elif(asc==90):
+		print('C',end="")
