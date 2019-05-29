a57b84667ffa123ef54c64fa2e39fca2c9ecb82b
saipoojavr <50616206+saipoojavr@users.noreply.github.com>
2019-05-29 16:59:38 +0530

    Add files via upload

---
+++ island.py
@@ -0,0 +1,30 @@
+try:
+	n=int(input())
+	l=[]
+	count=0 
+	for i in range(n):
+	    l.append(list(map(int,input().split())))
+	if n==1 and  l[0]==[1]:
+	     print('1')
+	else:
+	    for i in range(n):
+	        for j in range(n):
+	            if l[i][j]==1:
+	                if i==0 and j==n-1:
+	                    if(l[i][j-1]==0 and l[i+1][j]==0):
+	                            count+=1
+	                elif i==0 and j==0:
+	                    if l[i][j+1]==0 and l[i+1][j]==0:
+	                        count+=1
+	                elif i==0 and l[i][j-1]==0 and l[i][j+1]==0 and l[i+1][j]==0:
+	                    count+=1
+	                elif i==n-1 and j==n-1:
+	                    if l[i-1][j]==0 and l[i][j-1]==0:
+	                        count+=1
+	                elif i==n-1 and l[i][j-1]==0 and l[i][j+1]==0 and l[i-1][j]==0:
+	                    count+=1
+	                elif i!=0 or i!=n-1 and (l[i][j+1]==0 and l[i+1][j]==0 and l[i-1][j]==0 and l[i][j-1]==0):
+	                    count+=1
+	    print(count)    
+except ValueError:
+	print("invalid")
