d05985bae7945b83318550e010d9639bd78a034b
saipoojavr <50616206+saipoojavr@users.noreply.github.com>
2019-05-31 12:55:41 +0530

    Update periar.py

--- periar.py
+++ periar.py
@@ -2,8 +2,17 @@ try:
 	peri,ar=map(int,input().split())
 	h=int(peri/2)
 	if(h*2==peri):
-		print("yes")
+		if ar > 1:
+			for iter in range(2, ar//2):
+				 if (ar % iter) == 0: 
+				 	print("yes")
+				 	break
+			else:
+				print("no")
+		else:
+			print("yes")
 	else:
 		print("no")
 except ValueError:
 	print("invalid")
+	
