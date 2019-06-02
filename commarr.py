b71f0406b4786208ee8e55ba7a4f0ff56ac191da
saipoojavr <50616206+saipoojavr@users.noreply.github.com>
2019-06-02 22:06:56 +0530

    Update commarr.py

--- commarr.py
+++ commarr.py
@@ -1,10 +1,14 @@
 try:
 	num=int(input())
-	arr1=list(map(int,input().split()))
-	arr2=list(map(int,input().split()))
-	for i in range(0,len(arr1)):
-		for j in range(0,len(arr2)):
-			if(arr1[i]==arr2[j]):
-				print(arr1[i],end=" ")
+	list3=[]
+	s=""
+	list1=list(map(int,input().split()))
+	list2=list(map(int,input().split()))
+	for i in list1:
+		if i in list2 and i not in list3:
+			list3.append(i)
+	for i in list3:
+		s=s+str(i)+" "
+	print(s.strip())	
 except ValueError:
 	print("invalid")
