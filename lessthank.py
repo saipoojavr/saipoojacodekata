cc14bf22f22826ac0cca10851b2e82876751d1c8
saipoojavr <50616206+saipoojavr@users.noreply.github.com>
2019-06-02 22:40:22 +0530

    Update lessthank.py

--- lessthank.py
+++ lessthank.py
@@ -1,8 +1,12 @@
 try:
 	num1,num2=map(int,input().split())
 	arr=list(map(int,input().split()))
+	a=[]
 	for i in range(0,num1):
 		if(arr[i]<num2):
-			print(arr[i],end=" ")
+			a.append(arr[i])
+	a.sort()
+	for i in range(0,len(a)):
+		print(a[i],end=" ")
 except ValueError:
 	print("invalid")
