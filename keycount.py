34c82b7f3bd7eb64b3d62435f571e421e5d97348
saipoojavr <50616206+saipoojavr@users.noreply.github.com>
2019-06-02 23:17:13 +0530

    Add files via upload

---
+++ keycount.py
@@ -0,0 +1,15 @@
+try:
+	num1,num2=map(int,input().split())
+	arr=list(map(int,input().split()))
+	count=1
+	for i in range(0,num1-1):
+		for j in range(i+1,num1):
+			if(arr[i]==arr[j]):
+				count+=1
+		if(count==num2):
+			print(arr[i])
+		else:
+			count=1
+			continue
+except ValueError:
+	print("invalid")
