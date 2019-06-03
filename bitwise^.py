63c40727ee8e0247349b259eb49d0d0cd7bfc2a7
saipoojavr <50616206+saipoojavr@users.noreply.github.com>
2019-06-03 23:40:21 +0530

    Update bitwise^.py

--- bitwise^.py
+++ bitwise^.py
@@ -1,9 +1,8 @@
 try:
 	num=int(input())
-	arr=[int(x) for x in input().split()]
-	a=arr[0]
-	for i in range(1,len(arr)):
-		a=a^arr[i]
-	print(a)
+	arr=[int(num) for num in input().split()]
+	for i in range(0,len(arr)-1):
+	    xor=arr[i]^arr[i+1]
+	print(xor)
 except ValueError:
 	print("invalid")
