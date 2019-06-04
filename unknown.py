64552d4bee1d63f4d2c924538a67afc2a5f035c7
saipoojavr <50616206+saipoojavr@users.noreply.github.com>
2019-06-04 22:22:49 +0530

    Update unknown.py

--- unknown.py
+++ unknown.py
@@ -2,9 +2,7 @@
 try:
 	num=int(input())
 	arr=list(map(int,input().split()))
-	sum=0
-	for i in range(0,num):
-		sum=sum+arr[i]
-	print(sum*2)
+	arr.sort()
+	print(2*(arr[-1]+arr[-2]))
 except ValueError:
 	print("invalid")
