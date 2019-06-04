a125557b96f9dbc768c965ba637fbda6b1c3afdd
saipoojavr <50616206+saipoojavr@users.noreply.github.com>
2019-06-04 20:03:17 +0530

    Update maxnumsum.py

--- maxnumsum.py
+++ maxnumsum.py
@@ -3,12 +3,13 @@ try:
 	arr=list(map(int,input().split()))
 	sum=0
 	a=[]
-	for i in range(0,num-1,2):
+	for i in range(0,num-1):
 		if(arr[i]<arr[i+1]):
 			a.append(arr[i+1])
 		else:
 			a.append(arr[i])
 	for j in range(0,len(a)):
 		sum=sum+a[j]
+	print(sum)
 except ValueError:
 	print("invalid")
