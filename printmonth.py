f1f87972e4ec47a33c512bd2ed4f8cb960f0f586
saipoojavr <50616206+saipoojavr@users.noreply.github.com>
2019-06-06 16:57:47 +0530

    Add files via upload

---
+++ printmonth.py
@@ -0,0 +1,25 @@
+date=map(str,input().split("-"))
+if date[1]=="01":
+	print("January")
+elif date[1]=="02":
+	print("February")
+elif date[1]=="03":
+	print("March")
+elif date[1]=="04":
+	print("April")
+elif date[1]=="05":
+	print("May")
+elif date[1]=="06":
+	print("June")
+elif date[1]=="07":
+	print("July")
+elif date[1]=="08":
+	print("August")
+elif date[1]=="09":
+	print("September")
+elif date[1]=="10":
+	print("October")
+elif date[1]=="11":
+	print("November")
+elif date[1]=="12":
+	print("December")
