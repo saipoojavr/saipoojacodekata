eb1a3be838b2955e71a0cbcd905fd64b6051498a
saipoojavr <50616206+saipoojavr@users.noreply.github.com>
2019-06-07 00:23:11 +0530

    Add files via upload

---
+++ unknown3.py
@@ -0,0 +1,15 @@
+try:
+	n,k=map(int,input().split())
+	arr=[]
+	count=0
+	for i in range(0,n):
+	    arr.append(list(map(int,input().split())))
+	for i in range(0,len(arr)):
+	    if arr[i][1]==k:
+	        count+=1
+	if count==0:
+	    print("no")
+	else:
+	    print("yes")
+ecept ValueError:
+	print("invalid")
