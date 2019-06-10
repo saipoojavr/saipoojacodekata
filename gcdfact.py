81b0ae7e9620095bd02f3976166ae85b30df047f
saipoojavr <50616206+saipoojavr@users.noreply.github.com>
2019-06-11 00:42:00 +0530

    Add files via upload

---
+++ gcdfact.py
@@ -0,0 +1,11 @@
+from math import gcd
+try:
+	def fact(x):
+		f=1
+		for i in range(1,x+1):
+			f=f*i
+		return f
+	num1,num2=map(int,input().split())
+	print(gcd(fact(num1),fact(num2)))
+except ValueError:
+	print("invalid")
