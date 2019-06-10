2962df5f007910d72b8e45b828f8e2879a2e6cad
saipoojavr <50616206+saipoojavr@users.noreply.github.com>
2019-06-10 23:09:08 +0530

    Add files via upload

---
+++ 1'scount.py
@@ -0,0 +1,11 @@
+try:
+	n1,n2=map(int,input().split())
+	num=n1*n2
+	con=bin(num)[2::]
+	c=0
+	for i in range(0,len(con)):
+		if(con[i]=="1"):
+			c+=1
+	print(c)
+except ValueError:
+	print("invalid")
