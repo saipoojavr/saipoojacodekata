d3e93870a0f142f9bb3140d81287d7b2ecd47162
saipoojavr <50616206+saipoojavr@users.noreply.github.com>
2019-06-11 00:06:21 +0530

    Update searchmore.py

--- searchmore.py
+++ searchmore.py
@@ -2,8 +2,15 @@ try:
 	num,key=map(int,input().split())
 	arr=list(map(int,input().split()))
 	a=sorted(arr)
-	for i in range(0,len(a)):
-		if a[i]==key:
-			print(a[i+1])
+	for iter in range(0,num):
+		if a[iter]==key:
+			print(a[iter+1])
+			break
+	else:
+		a.append(key)
+		a.sort()
+		for i in range(0,len(a)):
+			if a[i]==key:
+				print(a[i+1])
 except ValueError:
 	print("invalid")
