4239f59478440cacce8a02efea58d4f9c0c8b041
saipoojavr <50616206+saipoojavr@users.noreply.github.com>
2019-06-13 22:31:36 +0530

    Update primelen.py

--- primelen.py
+++ primelen.py
@@ -4,8 +4,8 @@ for i in range(0,len(astr)):
 	if(astr[i]!=" "):
 		count+=1
 if count > 1: 
-	   for iter in range(2, count//2): 
-	       if (count % iter) == 0: 
+	   for iter in range(2, count//2+1): 
+	       if ((count % iter) == 0): 
 	           print("no") 
 	           break
 	   else: 
