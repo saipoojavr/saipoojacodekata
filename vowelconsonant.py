a3c650e03e1a9c20df56ad3449137a53a8a24c48
saipoojavr <50616206+saipoojavr@users.noreply.github.com>
2019-06-19 17:01:51 +0530

    Add files via upload

---
+++ vowelconsonant.py
@@ -0,0 +1,7 @@
+astr=input()
+for i in astr:
+	if i=="a" or i=="e" or i=="i" or i=="o" or i=="u":
+		print(i,end="")
+for i in astr:
+	if i!="a" and i!="e" and i!="i" and i!="o" and i!="u":
+		print(i,end="")
