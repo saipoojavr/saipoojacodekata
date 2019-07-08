74808b7774d2ecc2cd68112aa3ae2d3b4a03eb58
saipoojavr <50616206+saipoojavr@users.noreply.github.com>
2019-07-08 20:34:42 +0530

    Update largestno.py

--- largestno.py
+++ largestno.py
@@ -2,8 +2,15 @@ try:
 	num=int(input())
 	arr=list(map(int,input().split()))
 	arr.sort()
-	for i in range(len(arr),0,-1):
-		print(i,end="")
+	c=0
+	for i in range(0,num):
+		if(arr[i]==0):
+			c+=1
+	if(c==num):
+		print("0")
+	else:
+		for i in range(len(arr)-1,-1,-1):
+			print(arr[i],end="")
 except ValueError:
 	print("invalid")
 	
