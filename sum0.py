55091fc83832b52ed941e1026cdb74d01d316a59
saipoojavr <50616206+saipoojavr@users.noreply.github.com>
2019-07-10 23:25:00 +0530

    Update sum0.py

--- sum0.py
+++ sum0.py
@@ -1,11 +1,15 @@
-try:
-	num=int(input())
-	arr=list(map(int,input().split()))
-	for i in range(0,num-1):
-		for j in range(i+1,num):
-			s=arr[i]+arr[j]
-			if(s==0):
-				print(arr[i], arr[j])
-				break
-except ValueError:
-	print("invalid")
+num=int(input())
+arr=list(map(int,input().split()))
+a=[]
+for i in range(0,num-1):
+	for j in range(i+1,num):
+		s=arr[i]+arr[j]
+		if(s<=0):
+			a.append(s)
+s1=min(a)
+for i in range(0,num-1):
+	for j in range(i+1,num):
+		s2=arr[i]+arr[j]
+		if(s2==s1):
+			print(arr[i], arr[j])
+			break
