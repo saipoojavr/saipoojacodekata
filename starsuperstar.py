8b97b6377d746addda35bd34379695c43f86dd28
saipoojavr <50616206+saipoojavr@users.noreply.github.com>
2019-07-22 22:03:01 +0530

    Update starsuperstar.py

--- starsuperstar.py
+++ starsuperstar.py
@@ -1,10 +1,11 @@
 try:
 	num=int(input())
 	arr=list(map(int,input().split()))
+	a=[]
 	for i in range(0,num):
 		if arr[i]==max(arr[i::]):
-			print(arr[i],end=" ")
-	print("\r")
+			a.append(arr[i])
+	print(*a)
 	for i in range(num-1,-1,-1):
 		if arr[i]==max(arr):
 			print(arr[i],end=" ")
